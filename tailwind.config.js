9ed3a6626c5292a4a336253a915487d198f6dcaa
Dathan Stone <developer.stone@outlook.com>
2021-11-03 20:36:05 -0700

    Edited the Tailwind.config to include a dark gold hex code. Applied the dark gold hex code to a hover effect on the navbar list items, and made the h1 logo on the left side of the navbar to be the dark gold color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-     
+      textColor: theme => theme('colors'),
+     textColor: {
+      "darkGold": '#EEBC1D'
+     }
+    
     },
   },
   variants: {
