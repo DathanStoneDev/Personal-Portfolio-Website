529b03d44339191ff33238ddaca2bc2aba151b37
Dathan Stone <developer.stone@outlook.com>
2021-11-07 21:37:48 -0700

    Starting to refactor code slightly. Using custom Container size to keep everything within the same size containers. Changing color scheme to be a blue instead of dark gold.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      "darkGold": '#EEBC1D'
+      "primary": '#2C93FC',
+      "secondary": '#FBFBFB',
+      "almost-black": '#504F54'
      },
      fontFamily: {
-       "Roboto": ['Roboto', 'sans-serif']
+       "Roboto": ['Roboto', 'sans-serif'],
+       "Montserrat": ['Montserrat', 'sans-serif']
      }
     },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: "1280px",
+        xl: "1280px",
+        "2xl": "1280px"
+      }
+    }
   },
   variants: {
     extend: {},
